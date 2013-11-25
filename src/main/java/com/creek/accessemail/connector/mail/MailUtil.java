9d10928f9f7b28741ddac63186b57a95258b52b1
Andrey Pereverzin <bertoluCCi>
2013-11-25 20:57:22 +0000

    Added check for email == null

--- src/main/java/com/creek/accessemail/connector/mail/MailUtil.java
+++ src/main/java/com/creek/accessemail/connector/mail/MailUtil.java
@@ -10,19 +10,37 @@ public final class MailUtil {
     private static final String AT = "@";
 
     public static String extractUsernameFromEmailAddress(String emailAddress) {
+        if (emailAddress == null) {
+            return "";
+        }
+        
         StringTokenizer st = new StringTokenizer(emailAddress, AT);
         if (st.countTokens() == 2) {
             return st.nextToken();
         }
+        
         return emailAddress;
     }
     
     public static String extractHostnameFromEmailAddress(String emailAddress) {
+        if (emailAddress == null) {
+            return "";
+        }
+        
         StringTokenizer st = new StringTokenizer(emailAddress, AT);
         if (st.countTokens() == 2) {
             st.nextToken();
             return st.nextToken();
         }
+
         return null;
+    }
+    
+    public static boolean isEmailAddressValid(String emailAddress) {
+        return emailAddress != null && !emailAddress.contains(" ");
+    }
+    
+    public static boolean isEmailUsernameValid(String emailUsername) {
+        return emailUsername != null && !emailUsername.contains(AT);
     }
 }
